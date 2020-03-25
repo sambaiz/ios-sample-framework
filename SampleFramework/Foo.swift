9662d04a6fe367a927297c0150793d3afcdcf080
sambaiz <godgourd@gmail.com>
2020-03-25 15:48:15 +0900

    Initial commit

---
+++ SampleFramework/Foo.swift
@@ -0,0 +1,13 @@
+//
+//  Foo.swift
+//  SampleFramework
+//
+//  Created by Taiki Sakamoto on 2020/03/25.
+//  Copyright © 2020 sambaiz. All rights reserved.
+//
+
+import Foundation
+
+@objc func Foo() -> String {
+    return "FOO!"
+}
---
+++ SampleFramework/SampleFramework.h
@@ -0,0 +1,19 @@
+//
+//  SampleFramework.h
+//  SampleFramework
+//
+//  Created by Taiki Sakamoto on 2020/03/25.
+//  Copyright © 2020 sambaiz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleFramework.
+FOUNDATION_EXPORT double SampleFrameworkVersionNumber;
+
+//! Project version string for SampleFramework.
+FOUNDATION_EXPORT const unsigned char SampleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleFramework/PublicHeader.h>
+
+
